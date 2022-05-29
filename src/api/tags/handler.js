671ed51501e9f568bc6c7bf3ad5f9af30d030039
MahardikaNataprawira <yorunokovsinger@gmail.com>
2022-05-29 21:05:52 +0700

    added tagging service

--- src/api/tags/handler.js
+++ src/api/tags/handler.js
@@ -1,1 +1,125 @@
-// TODO: create handlers for tagging endpoint
+const ClientError = require('../../exceptions/ClientError')
+
+class TagsHandler {
+  constructor (tagService) {
+    this._tagService = tagService
+  }
+
+  async getTagByFactHandler (request, h) {
+    try {
+      const { factId } = request.params
+      const tags = await this._tagService.getTagByFact({ factId })
+      const response = h.response({
+        status: 'success',
+        message: 'successfully get tags',
+        data: {
+          tags
+        }
+      })
+      return response
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'failed',
+          message: error.message
+        })
+        response.code(error.statusCode)
+        return response
+      }
+      // server error
+      const response = h.response({
+        status: 'fail',
+        message: 'Internal Server Error'
+      })
+      response.code(500)
+      console.error(error)
+      return response
+    }
+  }
+
+  async addTagHandler (request, h) {
+    try {
+      const { factId, tagId } = request.payload
+      await this._tagService().addTag({ factId, tagId })
+
+      const response = h.response({
+        status: 'success',
+        message: 'Tag successfully added'
+      })
+
+      return response
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'failed',
+          message: error.message
+        })
+        response.code(error.statusCode)
+        return response
+      }
+      // server error
+      const response = h.response({
+        status: 'fail',
+        message: 'Internal Server Error'
+      })
+      response.code(500)
+      console.error(error)
+      return response
+    }
+  }
+
+  async updateTagHandler (request, h) {
+    try {
+      // TODO
+
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'failed',
+          message: error.message
+        })
+        response.code(error.statusCode)
+        return response
+      }
+      // server error
+      const response = h.response({
+        status: 'fail',
+        message: 'Internal Server Error'
+      })
+      response.code(500)
+      console.error(error)
+      return response
+    }
+  }
+
+  async deleteTagHandler (request, h) {
+    try {
+      const { tag } = request.params
+      await this._tagService.deleteTagById({ tag })
+      const response = h.response({
+        status: 'success',
+        message: 'successfully deleted tag'
+      })
+      return response
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'failed',
+          message: error.message
+        })
+        response.code(error.statusCode)
+        return response
+      }
+      // server error
+      const response = h.response({
+        status: 'fail',
+        message: 'Internal Server Error'
+      })
+      response.code(500)
+      console.error(error)
+      return response
+    }
+  }
+}
+
+module.exports = TagsHandler
--- src/api/tags/routes.js
+++ src/api/tags/routes.js
@@ -1,10 +1,29 @@
 const routes = (handler) => [
   {
     method: 'GET',
-    path: '/tag/$fact_id',
+    path: '/tag/get/$fact_id',
     handler: handler.getTagsByFactHandler
+  },
+  {
+    method: 'GET',
+    path: '/tag/get/$tag_id',
+    handler: handler.getTagsByTagHandler
+  },
+  {
+    method: 'POST',
+    path: '/tag/',
+    handler: handler.addTagHandler
+  },
+  {
+    method: 'PUT',
+    path: '/tag/update/$id',
+    handler: handler.updateTagHandler
+  },
+  {
+    method: 'DELETE',
+    path: '/tag/delete/$fact_id',
+    handler: handler.deleteTagHandler
   }
-  // TODO create more endpoints for tagging
 ]
 
 module.exports = routes
